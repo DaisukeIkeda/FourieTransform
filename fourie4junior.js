02632b5b7a25f02623dd049d1fffe2785669e0b4
Daisuke Ikeda <daisuke@inf.kyushu-u.ac.jp>
2014-04-06 04:57:52 +0900

    基本ベクトル用の 8x16 のマス目まで完成

---
+++ fourie4junior.js
@@ -0,0 +1,115 @@
+$(function() {
+    "use strict";
+
+    var mouse_tracking;
+    var touch_tracking = [];
+
+    // http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+    function is_touch_device() {
+        return !!('ontouchstart' in window) // works on most browsers
+            || !!('onmsgesturechange' in window); // works on ie10
+    }
+
+    function on_panel_mousedown(e) {
+        mouse_tracking = {
+            target: this,
+            initial_panel_color: $(this).hasClass("white") ? "white" : "black"
+        };
+	//console.log($("#palette .color.selected"));
+	//console.log($("#palette .color.selected").data("code"));
+	//console.log(typeof $("#palette .color.selected").data("code"));
+	$(this).attr("data-color-code", 1);
+    }
+
+    function on_panel_mousemove(e) {
+        if (mouse_tracking && mouse_tracking.target !== this) {
+	    $(this).attr("data-color-code", 2);
+            mouse_tracking.target = this;
+        }
+    }
+
+    function on_panel_mouseup(e) {
+        mouse_tracking = null;
+    }
+
+    function on_panel_touchstart(e) {
+        $.each(e.originalEvent.changedTouches, function(_, t) {
+            var target = document.elementFromPoint(t.clientX, t.clientY);
+            if ($(target).hasClass('panel')) {
+                touch_tracking[t.identifier] = {
+                    target: target,
+                    initial_panel_color: $(target).hasClass("white") ? "white" : "black"
+                };
+		$(target).attr("data-color-code", 3);
+            }
+        });
+    }
+
+    function on_panel_touchmove(e) {
+        $.each(e.originalEvent.changedTouches, function(_, t) {
+            var target = document.elementFromPoint(t.clientX, t.clientY);
+            if ($(target).hasClass('panel')) {
+                if (touch_tracking[t.identifier] && touch_tracking[t.identifier].target !== target) {
+		    $(target).attr("data-color-code", 4);
+
+                    touch_tracking[t.identifier].target = target;
+                }
+            }
+        });
+    }
+
+    function on_panel_touchend(e) {
+        $.each(e.originalEvent.changedTouches, function(_, t) {
+            touch_tracking[t.identifier] = null;
+        });
+    }
+
+    // palette
+    i=0;
+    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
+    i=1;
+    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
+    i=2;
+    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
+
+    // board
+    for (var i = 1; i <= 16; ++i) {
+        var row_holder = $('<div class="row-holder"></div>');
+        var row = $('<div id="row' + i + '" class="row"></div>');
+        for (var j = 1; j <= 16; ++j) {
+            var cell = $('<div id="cell' + i + '-' + j + '" class="cell"></div>');
+	    var panel = $('<div id="panel' + i + '-' + j + '" class="panel"></div>');
+            panel.appendTo(cell);
+            cell.appendTo(row);
+
+	    if ( i == 8 ) // 横軸の描画
+		$('<style>#panel' + i + '-' + j + '{ border-bottom: 2px solid black}</style>').appendTo('head');
+
+        }
+        row.appendTo(row_holder);
+        row_holder.appendTo('#board');
+	/* 軸の名前*/
+        var num_str = (i <= 8 ? '' + 9 - i : '' + 8 - i);
+        $('<style>#row' + i + ':before { content: "' + num_str + '"; }</style>').appendTo('head');
+    }
+
+    function enable_edit() {
+        if (is_touch_device()) {
+            $(".panel").each(function() {
+                $(this).on('touchstart', on_panel_touchstart);
+                $(this).on('touchmove', on_panel_touchmove);
+                $(this).on('touchend', on_panel_touchend);
+            });
+        }
+        else {
+            $(".panel").each(function() {
+                $(this).on('mousedown', on_panel_mousedown);
+                $(this).on('mousemove', on_panel_mousemove);
+                $(this).on('mouseup', on_panel_mouseup);
+            });
+        }
+        $(".edit-button").addClass("on");
+    }
+
+    enable_edit();
+});
