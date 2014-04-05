5129bccffb517bf8b5b8e9a0cc9c708f0c611b44
Daisuke Ikeda <daisuke@inf.kyushu-u.ac.jp>
2014-04-06 05:01:49 +0900

    送信ブロック、基本ブロック、受信ブロックの 3 つのパネルを作成

--- fourie4junior.js
+++ fourie4junior.js
@@ -23,7 +23,7 @@ $(function() {
 
     function on_panel_mousemove(e) {
         if (mouse_tracking && mouse_tracking.target !== this) {
-	    $(this).attr("data-color-code", 2);
+	    $(this).attr("data-color-code", 1);
             mouse_tracking.target = this;
         }
     }
@@ -40,7 +40,7 @@ $(function() {
                     target: target,
                     initial_panel_color: $(target).hasClass("white") ? "white" : "black"
                 };
-		$(target).attr("data-color-code", 3);
+		$(target).attr("data-color-code", 1);
             }
         });
     }
@@ -50,7 +50,7 @@ $(function() {
             var target = document.elementFromPoint(t.clientX, t.clientY);
             if ($(target).hasClass('panel')) {
                 if (touch_tracking[t.identifier] && touch_tracking[t.identifier].target !== target) {
-		    $(target).attr("data-color-code", 4);
+		    $(target).attr("data-color-code", 1);
 
                     touch_tracking[t.identifier].target = target;
                 }
@@ -64,19 +64,11 @@ $(function() {
         });
     }
 
-    // palette
-    i=0;
-    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
-    i=1;
-    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
-    i=2;
-    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
-
-    // board
+    // inputboard & outputboard
     for (var i = 1; i <= 16; ++i) {
         var row_holder = $('<div class="row-holder"></div>');
         var row = $('<div id="row' + i + '" class="row"></div>');
-        for (var j = 1; j <= 16; ++j) {
+        for (var j = 1; j <= 4; ++j) {
             var cell = $('<div id="cell' + i + '-' + j + '" class="cell"></div>');
 	    var panel = $('<div id="panel' + i + '-' + j + '" class="panel"></div>');
             panel.appendTo(cell);
@@ -87,29 +79,63 @@ $(function() {
 
         }
         row.appendTo(row_holder);
-        row_holder.appendTo('#board');
+        row_holder.appendTo('.board');
+
+	// board4base
+        row_holder = $('<div class="row-holder2"></div>');
+        row = $('<div id="row' + i + '" class="row"></div>');
+        for (var j = 1; j <= 16; ++j) {
+            var cell = $('<div id="cell' + i + '-' + j + '" class="cell"></div>');
+	    var panel = $('<div id="panel' + i + '-' + j + '" class="panel"></div>');
+            panel.appendTo(cell);
+            cell.appendTo(row);
+
+	    if ( i == 8 ){ // 横軸と縦軸の描画
+		if ( j % 4 == 0 )
+		    $('<style>#panel' + i + '-' + j + '{ border-bottom: 2px solid black; border-right: 1px solid black}</style>').appendTo('head');
+		else
+		    $('<style>#panel' + i + '-' + j + '{ border-bottom: 2px solid black}</style>').appendTo('head');
+	    }else if ( j % 4 == 0){
+		    $('<style>#panel' + i + '-' + j + '{ border-right: 1px solid black}</style>').appendTo('head');
+	    }
+
+        }
+        row.appendTo(row_holder);
+        row_holder.appendTo('.board2');
+
 	/* 軸の名前*/
         var num_str = (i <= 8 ? '' + 9 - i : '' + 8 - i);
         $('<style>#row' + i + ':before { content: "' + num_str + '"; }</style>').appendTo('head');
     }
 
-    function enable_edit() {
-        if (is_touch_device()) {
-            $(".panel").each(function() {
-                $(this).on('touchstart', on_panel_touchstart);
-                $(this).on('touchmove', on_panel_touchmove);
-                $(this).on('touchend', on_panel_touchend);
-            });
-        }
-        else {
-            $(".panel").each(function() {
-                $(this).on('mousedown', on_panel_mousedown);
-                $(this).on('mousemove', on_panel_mousemove);
-                $(this).on('mouseup', on_panel_mouseup);
-            });
-        }
-        $(".edit-button").addClass("on");
+    if (is_touch_device()) {
+        // disable the overscroll effect on touch-capable environments.
+	// http://www.html5rocks.com/en/mobile/touch/
+	// http://stackoverflow.com/questions/2890361/disable-scrolling-in-an-iphone-web-application
+	$(document).on('touchmove', function(e) {
+            e.preventDefault();
+	});
     }
 
-    enable_edit();
+    if (is_touch_device()) {
+        $(".panel").each(function() {
+            $(this).on('touchstart', on_panel_touchstart);
+            $(this).on('touchmove', on_panel_touchmove);
+            $(this).on('touchend', on_panel_touchend);
+        });
+    }
+    else {
+        $(".panel").each(function() {
+            $(this).on('mousedown', on_panel_mousedown);
+            $(this).on('mousemove', on_panel_mousemove);
+            $(this).on('mouseup', on_panel_mouseup);
+        });
+    }
+    $(".edit-button").addClass("on");
+
+    // style for black&white cells
+    i=0;
+    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: white; }</style>").appendTo("html > head");
+    i=1;
+    $("<style>.panel[data-color-code=\"" + i + "\"] { background-color: black; }</style>").appendTo("html > head");
 });
